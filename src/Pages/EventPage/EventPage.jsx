6ed45d23cd689e31de038b9fbd5700332edf8088
Moritz Makowski <moritz@dostuffthatmatters.dev>
2020-05-21 00:31:46 +0200

    Implement section structure of EventPage

--- src/Pages/EventPage/EventPage.jsx
+++ src/Pages/EventPage/EventPage.jsx
@@ -1,10 +1,56 @@
 
 import React from 'react';
+import {makeStyles} from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+
+import Section1Logo from "./Section1Logo";
+import Section2Slider from "./Section2Slider/Section2Slider";
+import Section3Map from "./Section3Map";
+
+const useStyles = makeStyles(theme => ({
+    fullWidthSection: {
+        width: "100vw",
+        paddingTop: theme.spacing(6),
+        paddingBottom: theme.spacing(6),
+    }
+}));
+
+
+
+function FullWidthSection(props) {
+
+    const classes = useStyles();
+
+    return (
+        <div style={{backgroundColor: props.color}} className={classes.fullWidthSection}>
+            <Container maxWidth="md">
+                {props.children}
+            </Container>
+        </div>
+    );
+}
+
 
 function EventPage (props) {
 
     return (
-        <div>EventPage</div>
+        <React.Fragment>
+            <FullWidthSection color={"rgb(200, 50, 50)"}>
+                Logo
+            </FullWidthSection>
+            <FullWidthSection color={"rgb(200, 200, 50)"}>
+                Slider
+            </FullWidthSection>
+            <FullWidthSection color={"rgb(200, 200, 200)"}>
+                Image 1: Lazy loading image! -> pixeled out :D (incl. optional copyright overlay)
+            </FullWidthSection>
+            <FullWidthSection color={"rgb(50, 200, 50)"}>
+                Map
+            </FullWidthSection>
+            <FullWidthSection color={"rgb(50, 50, 200)"}>
+                Image 2: Lazy loading image! -> pixeled out :D (incl. optional copyright overlay)
+            </FullWidthSection>
+        </React.Fragment>
     );
 }
 
